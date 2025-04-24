75aa01123b4244314c11e1b12e95d46386d67304
ChunkGlow <memoguy34@gmail.com>
2025-04-24 10:49:32 +0800

    Python

---
+++ 1/script.py
@@ -0,0 +1,13 @@
+empty_list = []
+print()
+
+number_list = [1, 2, 3, 4, 5]
+print(number_list)
+
+triple_list = [1, 2, 3] * 3
+print(triple_list)
+
+aList = [1, 2, 3, 4, 5]
+aList = aList[::-1]
+
+print(aList, "\n")
